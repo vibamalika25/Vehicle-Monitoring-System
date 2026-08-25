4ed72a23a9c5da1232d30145a15220c47a3988a6
vibamalika25 <125353636+vibamalika25@users.noreply.github.com>
2026-08-25 08:52:04 +0530

    Add files via upload

---
+++ frontend/build/static/js/updateHeaderText.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const headerElement = document.querySelector(
+    "#root > div > header > div > div.MuiTypography-root.MuiTypography-h6.MuiTypography-noWrap.css-1qhxpac"
+  );
+  if (headerElement) {
+    headerElement.textContent = "Sri Vinayaga Earth Movers";
+  }
+});
