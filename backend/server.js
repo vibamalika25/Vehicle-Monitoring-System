a71716750b011835581e8acaab21906b1b08a758
vibamalika25 <125353636+vibamalika25@users.noreply.github.com>
2026-08-26 14:15:21 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,12 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://vehicle-monitoring-system-1.onrender.com',
+  credentials: true
+}));
 app.use(express.json());
 
 // Email configuration
